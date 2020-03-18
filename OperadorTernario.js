5605ed71dee27552bec815c7bb59c4bca7db4f4c
mcristooball <43381590+mcristooball@users.noreply.github.com>
2020-03-18 10:54:46 -0600

    Create OperadorTernario.js

---
+++ OperadorTernario.js
@@ -0,0 +1,8 @@
+
+
+// Operador ternario: generar un if con un else en una sola linea
+// Sintaxis: condition ? true : false;
+
+var numero = 1;
+var resultado = numero === 1 ? "Soy un numero uno" : "No soy un numero uno"
+console.log(resultado);
