345c555a6b6dc7be60c294a2c57aa0c85213de26
mcristooball <43381590+mcristooball@users.noreply.github.com>
2020-03-19 08:38:00 -0600

    Update for.js

--- for.js
+++ for.js
@@ -5,7 +5,8 @@ function saludarAlumno (alumno){
     console.log(`Hola, ${alumno}`);
 };
 
-for (var i = 0; i < alumnos.length; i++) {
+for (var i = 0; i < alumnos.length; i++) { //Mientras la variable i sea menor a la longitud de 
+    // nuestro arreglo esa tarea se va a seguir generando.
     saludarAlumno(alumnos[i]);
 }
 
@@ -21,6 +22,7 @@ function saludarAlumno (alumno){
     console.log(`Hola, ${alumno}`);
 };
 
+// For of (Tenemos que inicializar una variable en singular de nuestro array que esta en plural)
 for(var alumno of alumnos){ //En esta linea hacemos referencia al singular del arreglo
     // donde la variable alumno cumple la funcion que desempeñaria el index.
     saludarAlumno(alumno);
