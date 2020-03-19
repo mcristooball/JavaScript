abb232c59b1ef04efb0c208cfacc913c348bee86
mcristooball <43381590+mcristooball@users.noreply.github.com>
2020-03-19 08:25:08 -0600

    Create for.js

---
+++ for.js
@@ -0,0 +1,14 @@
+
+var alumnos = ["Miguel", "Jocelyn", "Angel"];
+
+function saludarAlumno (alumno){
+    console.log(`Hola, ${alumno}`);
+};
+
+for (var i = 0; i < alumnos.length; i++) {
+    saludarAlumno(alumnos[i]);
+}
+
+>> Hola, Miguel
+>> Hola, Jocelyn
+>> Hola, Angel
