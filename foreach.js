2f2d1fd97f363dcb0cb3d5ebf1cdedafe8476f73
mcristooball <43381590+mcristooball@users.noreply.github.com>
2020-03-19 16:05:48 -0600

    Create foreach.js

---
+++ foreach.js
@@ -0,0 +1,20 @@
+
+
+var articulos = [
+     {nombre: "Impresora", costo:3200},
+     {nombre: "Lapicera", costo:100},
+     {nombre: "Celular", costo:10000},
+     {nombre: "Teclado", costo:500},
+     {nombre: "Mouse", costo:700}
+ ];
+
+var enlistaArticulo = articulos.forEach(function(articulo){
+    console.log(articulo.nombre);
+});
+
+
+>> Impresora
+>> Lapicera
+>> Celular
+>> Teclado
+>> Mouse
