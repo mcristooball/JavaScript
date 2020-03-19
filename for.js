503a4cad38b96eadf5f1c6719eaf6b5135908c8d
mcristooball <43381590+mcristooball@users.noreply.github.com>
2020-03-19 08:34:12 -0600

    Update for.js

--- for.js
+++ for.js
@@ -12,3 +12,16 @@ for (var i = 0; i < alumnos.length; i++) {
 >> Hola, Miguel
 >> Hola, Jocelyn
 >> Hola, Angel
+
+--------------------------------------------------------------------------
+
+var alumnos = ["Miguel", "Jocelyn", "Angel"];
+
+function saludarAlumno (alumno){
+    console.log(`Hola, ${alumno}`);
+};
+
+for(var alumno of alumnos){ //En esta linea hacemos referencia al singular del arreglo
+    // donde la variable alumno cumple la funcion que desempeñaria el index.
+    saludarAlumno(alumno);
+};
